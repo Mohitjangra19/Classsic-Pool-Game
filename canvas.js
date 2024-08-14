3195196e8dd54d93e7224c2e80bba5a38acc1472
Mohitjangra19 <mjsg46000@gmail.com>
2024-08-14 18:04:49 +0530

    assets folder added

--- canvas.js
+++ canvas.js
@@ -1,6 +1,6 @@
 function Canvas2D() {
     this._canvas = document.getElementById('screen');
-    this._canvasContext = this._canvas.getContext('2D');
+    this._canvasContext = this._canvas.getContext('2d');
 }
 
 
@@ -12,3 +12,15 @@ Canvas2D.prototype.clear = function() {
 Canvas2D.prototype.drawImage = function(image, position) {
     this._canvasContext.drawImage(image, position.x, position.y);
 }
+
+let Canvas = new Canvas2D();
+
+//Testing
+let image = new Image();
+image.src = "./assets/sprites/spr_background4.png"
+
+setTimeout(() => {
+Canvas.drawImage(image, {x:0,y:0});
+
+}, 1000);
+
